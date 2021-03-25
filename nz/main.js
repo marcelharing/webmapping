f93ce284c7314769931b854192028cc4956c5ce2
Marcel Haring <marcel.haring@student.uibk.ac.at>
2021-03-25 16:17:18 +0100

    generated map

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,9 @@
-console.log('Hallo Welt');
-console.log(L);
+
+const map = L.map("map", {
+    center: [-45.775, 170.727778],
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+console.log(document.querySelector("#map"));
