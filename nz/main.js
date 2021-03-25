d44b74d83c7cc4ff20e25a180e1d3df062b328e2
Marcel Haring <marcel.haring@student.uibk.ac.at>
2021-03-25 17:03:03 +0100

    popup with templatesyntax

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,27 @@
+let stop = {
+    nr: 8,
+    name: "Taiaroa Head",
+    lat: -45.775,
+    lng: 170.727778,
+    user: "marcelharing",
+    wikipedia: "https://en.wikipedia.org/wiki/Taiaroa_Head"
+}
+
+console.log(stop);
+console.log(stop.name);
 
 const map = L.map("map", {
-    center: [-45.775, 170.727778],
+    center: [stop.lat, stop.lng],
     zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 
-let mrk = L.marker([-45.775, 170.727778]).addTo(map);
-mrk.bindPopup("Taiaroa Head").openPopup();
-console.log(document.querySelector("#map"));
+let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
+mrk.bindPopup(`
+    <h4>Stop ${stop.nr}: ${stop.name}</h4>
+    <p><i class="fab fa-wikipedia-w marg_r3"></i><a href="${stop.wikipedia}">Read about stop on Wikipedia</a></p>
+    `).openPopup();
+
+//console.log(document.querySelector("#map"));
