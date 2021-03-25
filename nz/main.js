0efbc8a8ec6196b6cec567c974979283b43207ab
Marcel Haring <marcel.haring@student.uibk.ac.at>
2021-03-25 16:22:47 +0100

    added popup

--- nz/main.js
+++ nz/main.js
@@ -6,4 +6,7 @@ const map = L.map("map", {
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
+
+let mrk = L.marker([-45.775, 170.727778]).addTo(map);
+mrk.bindPopup("Taiaroa Head").openPopup();
 console.log(document.querySelector("#map"));
