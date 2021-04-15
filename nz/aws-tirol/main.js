f42b52de7e8c20834144ca0b711bca5b86ae8d2b
Marcel Haring <marcel.haring@student.uibk.ac.at>
2021-04-15 17:10:39 +0200

    add feature group

--- nz/aws-tirol/main.js
+++ nz/aws-tirol/main.js
@@ -1,6 +1,6 @@
 let basemapGray = L.tileLayer.provider('BasemapAT.grau')
 
-let map = L.map("map1", {
+let map = L.map("map", {
 center: [47, 11],
 zoom: 9,
 layers: [
@@ -21,4 +21,23 @@ let layerControl = L.control.layers({
 
 }).addTo(map)
 
-// https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+let awsLayer = L.featureGroup();
+layerControl.addOverlay(awsLayer, "Wetterstation Tirol");
+awsLayer.addTo(map);
+
+fetch(awsUrl)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertiert: ', json);
+        for (station of json.features) {
+            console.log('Station: ', station);
+            let marker = L.marker([
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0]
+            ]);
+            marker.bindPopup(`<h3>${station.properties.name}</h3>`);
+            marker.addTo(awsLayer);
+        }
+    });
