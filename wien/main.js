2049b68bc6bc6ca3019cc71891fdfa5d6395cecb
Marcel Haring <marcel.haring@student.uibk.ac.at>
2021-05-06 17:40:30 +0200

    font awesome icons corrected

--- wien/main.js
+++ wien/main.js
@@ -165,6 +165,21 @@ var miniMap = new L.Control.MiniMap(L.tileLayer.provider("BasemapAT.basemap"), {
 
 //reachability plugin
 L.control.reachability({
-    // add settings/options here
-    apiKey: "5b3ce3597851110001cf6248c8dd4e5431a048b19580e80f41d71ddf"
+    apiKey: "5b3ce3597851110001cf6248c8dd4e5431a048b19580e80f41d71ddf",
+    drawButtonContent: '',
+    drawButtonStyleClass: 'fa fa-pencil-alt',
+    deleteButtonContent: '',
+    deleteButtonStyleClass: 'fa fa-trash',
+    distanceButtonContent: '',
+    distanceButtonStyleClass: 'fa fa-road',
+    timeButtonContent: '',
+    timeButtonStyleClass: 'fa fa-clock',
+    travelModeButton1Content: '',
+    travelModeButton1StyleClass: 'fa fa-car',
+    travelModeButton2Content: '',
+    travelModeButton2StyleClass: 'fa fa-bicycle',
+    travelModeButton3Content: '',
+    travelModeButton3StyleClass: 'fa fa-male',
+    travelModeButton4Content: '',
+    travelModeButton4StyleClass: 'fa fa-wheelchair'
 }).addTo(map);
